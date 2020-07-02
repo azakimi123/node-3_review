6c742fd4f7e731ff5aa668c0a93e9d7c2d8feeea
azakimi123 <aaron.zakimi@gmail.com>
2020-07-02 10:06:48 -0600

    getItems and addItem functional

---
+++ db/add_item.sql
@@ -0,0 +1,7 @@
+INSERT INTO backpack_item (
+    item_name, item_purpose
+) VALUES (
+    ${itemName}, ${itemPurpose}
+);
+
+SELECT * FROM backpack_item;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const massive = require('massive');
 const app = express();
+const ctrl = require('./controllers/backpackCtrl');
 
 
 
+const { SERVER_PORT, DB_URI } = process.env;
 
+app.use(express.json());
 
-const SERVER_PORT = 5050;
+massive ({
+    connectionString: DB_URI,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then(db => {
+    app.set('db', db)
+    console.log('db is connected')
+}).catch(err => console.log(err));
+
+//Endpoints
+
+app.get('/api/backpackitems', ctrl.getItems);
+app.post('/api/backpackitems', ctrl.addItem); //use a body
+
 app.listen(SERVER_PORT, () => console.log(`Listening to sweet sounds on ${SERVER_PORT}FM`));
